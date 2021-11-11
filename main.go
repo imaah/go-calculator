b5373c79bf6757a24e91501bd2539d554762e8e9
Esteban Morisse <68004644+myimah@users.noreply.github.com>
2021-11-11 15:46:53 +0100

    Changing the exemple inside main

--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"emorisse.fr/calcul/operators/binary"
 	"emorisse.fr/calcul/operators/function"
 	"emorisse.fr/calcul/operators/number"
-	"emorisse.fr/calcul/operators/unary"
 	"fmt"
 )
 
 func main() {
-	u, _ := unary.New('-', number.New(10))
-	op, _ := binary.New('+', number.New(20), u)
+	// ((2 + 5) * cos(4)) / (sin(24) - (4 * 12))
+	tpf, _ := binary.New('+', number.New(2), number.New(5))
+	c4, _ := function.New("cos", number.New(4))
+	tpftc4, _ := binary.New('*', tpf, c4)
+	s24, _ := function.New("sin", number.New(24))
+	ftt, _ := binary.New('*', number.New(4), number.New(12))
+	s24mftt, _ := binary.New('-', s24, ftt)
 
-	fmt.Println(op.Eval())
+	div, _ := binary.New('/', tpftc4, s24mftt)
 
-	cos, _ := function.New("cos", number.New(0))
-	fmt.Println(cos.Eval())
+	fmt.Println(div.Eval())
 }
