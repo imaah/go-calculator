5e167ec9be19db328372283ad6da92309cfb171b
Esteban Morisse <68004644+myimah@users.noreply.github.com>
2021-11-12 20:17:21 +0100

    Adding a http server

--- main.go
+++ main.go
@@ -1,25 +1,22 @@
 package main
 
 import (
-	"emorisse.fr/calcul/operators/binary"
-	"emorisse.fr/calcul/operators/function"
-	"emorisse.fr/calcul/operators/number"
-	"emorisse.fr/calcul/parser"
-	"fmt"
+	"emorisse.fr/calcul/web"
 )
 
 func main() {
 	// ((2 + 5) * cos(4)) / (sin(24) - (4 * 12))
-	var tpf, _ = binary.New('+', number.New(2), number.New(5))
-	var c4, _ = function.New("cos", number.New(4))
-	var tpftc4, _ = binary.New('*', tpf, c4)
-	var s24, _ = function.New("sin", number.New(24))
-	var ftt, _ = binary.New('*', number.New(4), number.New(12))
-	var s24mftt, _ = binary.New('-', s24, ftt)
-
-	var div, _ = binary.New('/', tpftc4, s24mftt)
-	fmt.Println(div.Eval())
-
-	var res, _ = parser.Parse("((2 + 5) * cos(4)) / (sin(24) - (4 * 12))")
-	fmt.Println(res.Eval())
+	//var tpf, _ = binary.New('+', number.New(2), number.New(5))
+	//var c4, _ = function.New("cos", number.New(4))
+	//var tpftc4, _ = binary.New('*', tpf, c4)
+	//var s24, _ = function.New("sin", number.New(24))
+	//var ftt, _ = binary.New('*', number.New(4), number.New(12))
+	//var s24mftt, _ = binary.New('-', s24, ftt)
+	//
+	//var div, _ = binary.New('/', tpftc4, s24mftt)
+	//fmt.Println(div.Eval())
+	//
+	//var res, _ = parser.Parse("((2 + 5) * cos(4)) / (sin(24) - (4 * 12))")
+	//fmt.Println(res.Eval())
+	web.StartServer()
 }
---
+++ web/server.go
@@ -0,0 +1,106 @@
+package web
+
+import (
+	"emorisse.fr/calcul/operators"
+	"emorisse.fr/calcul/parser"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+var logger *log.Logger
+
+func init() {
+	logger = log.New(os.Stdout, "HTTP ", log.Flags())
+}
+
+type errorRes struct {
+	Error string `json:"error"`
+}
+
+type resultRes struct {
+	Result string `json:"result"`
+}
+
+type calculationReq struct {
+	Calculation string `json:"computation"`
+}
+
+func StartServer() {
+	var staticFolder = http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", staticFolder)
+	http.HandleFunc("/api/calculate", handleApiCalculate)
+
+	logger.Println("Starting server listening to port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		logger.Fatalln(err)
+	}
+}
+
+func handleApiCalculate(res http.ResponseWriter, req *http.Request) {
+	defer logger.Printf("%s %s\n", req.RemoteAddr, req.URL.Path)
+
+	// TODO : add more security, and input checking
+
+	res.Header().Set("Content-Type", "application/json")
+
+	fmt.Println(req.Method)
+
+	if req.Method == "POST" {
+		res.WriteHeader(200)
+
+		var operation, err = readCalculation(req.Body)
+
+		if err != nil {
+			sendJson(errorRes{Error: err.Error()}, res)
+			return
+		}
+
+		fmt.Println(operation)
+
+		var result = fmt.Sprintf("%f", operation.Eval())
+		sendJson(resultRes{result}, res)
+
+	} else {
+		res.WriteHeader(400)
+		sendJson(errorRes{"Bad Request"}, res)
+	}
+}
+
+func readCalculation(reader io.Reader) (operators.Operation, error) {
+	var content, err = ioutil.ReadAll(reader)
+	var operation operators.Operation
+
+	if err != nil {
+		return nil, err
+	}
+
+	var req calculationReq
+
+	err = json.Unmarshal(content, &req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(req.Calculation)
+	operation, err = parser.Parse(req.Calculation)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return operation, nil
+}
+
+func sendJson(elem interface{}, res http.ResponseWriter) {
+	jsonElem, _ := json.Marshal(elem)
+	_, _ = res.Write(jsonElem)
+}
